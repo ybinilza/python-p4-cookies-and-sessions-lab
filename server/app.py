bdac97cd3104aeb7adbc4bb4e0c1b9716f998f87
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-10-08 19:52:48 -0400

    Lab completed

--- server/app.py
+++ server/app.py
@@ -6,41 +6,45 @@ from flask_migrate import Migrate
 from models import db, Article, User
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = b"Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/clear')
+
+@app.route("/clear")
 def clear_session():
-    session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
+    session["page_views"] = 0
+    return {"message": "200: Successfully cleared session data."}, 200
 
-@app.route('/articles')
+
+@app.route("/articles")
 def index_articles():
-
     pass
-@app.route('/articles/<int:id>')
+
+
+@app.route("/articles/<int:id>")
 def show_article(id):
     article = Article.query.filter(Article.id == id).first().to_dict()
 
-    if session.get('page_views'):
+    if session.get("page_views"):
         session["page_views"] += 1
     else:
-        session['page_views'] = 1
-    
-    if session['page_views'] > 3:
-        response = make_response(jsonify({'message': 'Maximum pageview limit reached'}), 401)
+        session["page_views"] = 1
+
+    if session.get("page_views") > 3:
+        response = make_response(
+            jsonify({"message": "Maximum pageview limit reached"}), 401
+        )
     else:
         response = make_response(article, 200)
-        
+
     return response
 
-    pass
 
-if __name__ == '__main__':
-    app.run(port=5555)
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
